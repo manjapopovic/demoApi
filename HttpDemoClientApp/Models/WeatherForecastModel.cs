34c24d24a186a9848f11c474db81535d7171b3f2
Manja <manjaveca38@gmail.com>
2022-01-15 15:47:42 +0100

    initial commit

---
+++ HttpDemoClientApp/Models/WeatherForecastModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HttpDemoClientApp.Models
+{
+
+
+    public class WeatherForecastModel
+    {
+        public DayForecastModel[] Consolidated_weather { get; set; }
+        
+        public DateTime sun_rise { get; set; }
+        public DateTime sun_set { get; set; }
+         
+        public string Title { get; set; }
+       
+        public int woeid { get; set; }
+     
+        public string Timezone { get; set; }
+    }
+
+    
+
+}
